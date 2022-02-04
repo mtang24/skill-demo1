c16a41fd865b8546f8b1a3b1ad343ed1597f9603
mtang24 <michaeljtang18@gmail.com>
2022-02-03 17:45:39 -0800

    fixed failed code

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void subtraction(){
-        assertEquals(5, SkillDemo.subtract(11, 5)); // fails as expected
+        assertEquals(5, SkillDemo.subtract(11, 6)); 
     }
 }
